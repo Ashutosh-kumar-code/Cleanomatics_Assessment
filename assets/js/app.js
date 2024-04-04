c35710bed6320a8d26940f07e114caa1b57efb2c
Ashutosh-kumar-code <86124272+Ashutosh-kumar-code@users.noreply.github.com>
2024-04-04 21:56:57 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myproduct");
+    // li = ul.getElementsByTagName("div");
+    li = ul.getElementsByClassName("card_col")
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h5")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+  }
